3d16cdb05652902e94b27b986313c3344f8a954f
Kaitlyn Mirabella <kaitlynmirabella@Kaitlyns-MacBook-Air-3.local>
2020-11-20 18:04:23 -0500

    readded port

--- index.js
+++ index.js
@@ -224,6 +224,8 @@ app.delete('/student/:id', (req, res)=>{
     res.json(true); 
 });
 
-
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 
