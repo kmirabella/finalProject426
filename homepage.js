42c99ec26b9ecda4b1734723baa3257a326d3644
Peter Shelley <petershe@live.unc.edu>
2020-11-21 12:01:45 -0500

    login/signup requests handled completely in back and front

--- homepage.js
+++ homepage.js
@@ -2,72 +2,110 @@ $(function () {
     $('#signup-form').on('submit', (event) => handleSubmitSignup(event));
     $("#login-submit-button").on('click', (event) => handleLogin(event));
 });
-// const handleLoginSignups = function () {
-    
-// }
 
 async function handleLogin(e) {
     e.preventDefault();
     let email = $('#login-email').val();
     let password = $('#login-password').val();
+
+    $("#login-email").removeClass('is-danger');
+    $("#login-password").removeClass('is-danger');
+
+    if (email == "") {
+        $("#login-email").addClass('is-danger');
+        return;
+    }
+    if (password == "") {
+        $("#login-password").addClass('is-danger');
+        return;
+    }
+
     // try {
-    //     await axios({
-    //         method: 'POST',
-    //         url: "http://localhost:3000/signupPage.html",
-    //         data: {
-    //             "user": email,
-    //             "password": password
-    //         }
-    //     })
-    // } catch (err) {
-    //     let errorMessage = $('<h3>Username or password invalid.');
-    //     $('#login-password').append(errorMessage);
-    // }
+    // await axios({
+    //     method: 'POST',
+    //     url: "http://WWW.URL.COM/login",
+    //     data: {
+    //         "user": email,
+    //         "password": password
+    //     }
+    // })
 }
 
 async function handleSubmitSignup(e) {
     e.preventDefault();
-    let firstName = $('#firstName').val();
-    let lastName = $('#lastName').val();
+    let firstName = $('#first-name').val();
+    let lastName = $('#last-name').val();
     let email = $("#email").val();
     let pass = $("#password").val();
     let confirmedPassword = $("#confirm-password").val();
+
+    if ($("#signup-form").children()[9] != null) {
+        $("#signup-form").children()[9].remove();
+    }
+
+    $('#first-name').removeClass('is-danger');
+    $('#last-name').removeClass('is-danger');
+    $('#email').removeClass('is-danger');
+    $('#password').removeClass('is-danger');
+    $('#confirm-password').removeClass('is-danger');
+    
+    if (firstName == "") {
+        $('#first-name').addClass('is-danger'); 
+        return;
+    }
+    if (lastName == "") {
+        $('#last-name').addClass('is-danger');
+        return;
+    }
+    if (email == "") {
+        $('#email').addClass('is-danger');
+        return;
+    }
+    if (pass == "") {
+        $('#password').addClass('is-danger');
+        return;
+    }
+    if (confirmedPassword == "") {
+        $('#confirm-password').addClass('is-danger');
+        return;
+    }
+    if (!$("input[id='professor-radio']:checked").val() && !$("input[id='student-radio']:checked").val()) {
+        let errorMessage = $('<h1>Please indicate if you are a student or professor.</h1>');
+        $('#signup-form').append(errorMessage);
+        return;
+    }
     // front end password match confirmation
     if (pass == confirmedPassword) {
         // store our data here by creating a new professor or student object 
         if ($("input[id='professor-radio']:checked").val() == "on") {
-            //if checked professor
-            console.log('about to send and create backend Prof. obj');
-            await axios({
-                method: "POST",
-                url: "/professor",
-                data: {
-                    "firstName": firstName,
-                    "lastName": lastName,
-                    "email": email,
-                    "password": pass,
-                    "classes": [],
-                    "students": []
-                }
-            })
-            console.log(`signed up new user: ${firstName} ${lastName}`)
-        } else {
-            //if checked student
             // await axios({
-            //     method: "post",
-            //     url: "https://examscheduler2.netlify.app/student",
+            //     method: "POST",
+            //     url: "https://WWW.URL.COM/signup",
             //     data: {
             //         "firstName": firstName,
             //         "lastName": lastName,
             //         "email": email,
             //         "password": pass,
-            //         "classes": []
+            //         "professor": "true"
+            //     }
+            // })
+        } else {
+            //if checked student
+            // await axios({
+            //     method: "post",
+            //     url: "https://WWW.URL.COM/signup",
+            //     data: {
+            //         "firstName": firstName,
+            //         "lastName": lastName,
+            //         "email": email,
+            //         "password": pass,
+            //         "professor": "false"
             //     }
             // })
         }
 
     } else {
-        console.log("it was false! they didn't match")
-        $("#signup-form").append("<div class= passwordIncorrect><p><Strong>Passwords do not match, try again!<Strong></p></div>");
+        $('#password').addClass('is-danger');
+        $('#confirm-password').addClass('is-danger');
     }
 }
--- manage_classes.js
+++ manage_classes.js
@@ -1,18 +1,28 @@
-export function getClass(){
+$(function () {
+    loadFeed();
+});
+
+function renderAddClassForm(){
     let classHtml = '<span><div class="class_box" id="class_display"><h1>COMP426</h1><hr><h2>Exam Dates</h2><p>11/01/2020</p><p>9/12/2020</p><p>10/08/2020</p><p>8/24/2020</p><button type="submit" id="edit_button">edit</button><button type="submit">delete class</button></div></span>';
+
+    let $classesView = $('<div class="class_box" id="class_display"></div>');
+    let $classNameDropdown = $(`<label for="class-name">Class Name</labe><h1 id="class-name">${classname}</h1>`)
 
     return classHtml;
 }
 
-export function getClassEdit(){
+function getClassEdit(){
     let classEditHtml = '<span><div class="class_box" id="class_edit"><h1>Course Code</h1><input value="C426001"><hr><input value="11/01/2020"><span><button type="submit">delete</button></span><br><input value="9/12/2020"><span><button type="submit">delete</button></span><br><input value="10/08/2020"><span><button type="submit">delete</button></span><br><input value="8/24/2020"><span><button type="submit">delete</button></span><br><button type="submit">+ add date</button><button type="submit">Submit Changes</button><button type="submit" id="cancel_button">Cancel</button></div></span>';
 
     return classEditHtml;
 }
 
-export const loadFeed = async function () {
+const loadFeed = async function () {
+
+    // axios to pull student's classes with updated exam dates 
+    // and display them
     const $root = $('#root');
-    $root.append(getClass());
+    $root.append(renderAddClassForm());
 
     const loadEdit = function(){
         let classEditHtml = getClassEdit();
@@ -24,7 +34,7 @@ export const loadFeed = async function () {
     $root.on("click", '#edit_button', loadEdit);
 
     const loadClass = function(){
-        let classEditHtml = getClass();
+        let classEditHtml = renderAddClassForm();
         let oldClassDisplay = document.querySelector("#class_edit");
         const newClassEditDisplay = document.createElement("span");
         newClassEditDisplay.innerHTML = classEditHtml;
@@ -33,6 +43,22 @@ export const loadFeed = async function () {
     $root.on("click", '#cancel_button', loadClass);
 }
 
-$(function () {
-    loadFeed();
-});
+async function loadClass() {
+    /** axios to get class info
+     * let req = await axios({
+        method: 'GET',
+        url: "http://WWW.URL.COM/student/:id/"
+    })
+     */
+    //classname
+    //let classes = req.body.classes; as an array of class objects
+
+    let classView = $("<div class='box '></div>")
+                    .css('display', 'flex')
+                    .css('flex-flow', 'column-wrap');
+    // classes.forEach((c) => {
+    //     $("<")
+    // })
+    //exams
+}
+
