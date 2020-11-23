0220a96949a7b34e82d288978a94fa5b52f29926
Peter Shelley <petershe@live.unc.edu>
2020-11-23 03:57:52 -0500

    changes to layout + logout

--- manage_classes.js
+++ manage_classes.js
@@ -1,4 +1,5 @@
 const $root = $("#root");
+let counter = 0;
 let class1 = {
     name: "COMP110.001",
     exam_dates: [new Date(2020, 10, 17), new Date(2020, 8, 30)]
@@ -7,119 +8,91 @@ let class2 = {
     name: "COMP544.001",
     exam_dates: [new Date(2020, 10, 13), new Date(2020, 9, 7)]
 }
-let classes = [class1, class2];
+let classes = [];
 
 $(renderStudentView());
 
 async function renderStudentView() {
+
     // let req = await axios({
     //     method: "GET",
-    //     url: "https://comp426backend.herokuapp.com/student/:id"
-    //     //login credentials ?
-    // })
+    //     url: "https://comp426backend.herokuapp.com/student",
+    //     withCredentials: true,
+    // });
+    //console.log(req);
+
     //req.body.classes.forEach((c) => classes.push(c));
 
     //render student's existing classes and exam dates
     // assign global 'classes' var to the correct student's classes
     if (classes.length > 0) {
         classes.forEach((c) => renderClassView(c));
+    } else {
+        $("#add-view").append(renderAddForm());
     }
-    $("#new-class-button").on('click', renderAddClassForm);
-    //load autocomplete functionality for class selection
+}
+
+function renderAddForm() {
+    let formView = $('<div class="class_box" id="class_display"></div>');
+    let form = $("<form id='add-info-form'></form>");
+    let autocomplete = $(`<input class="input" type="text" id="className" placeholder="Enter Class">`).css('width', '700px');
+    let addAutoButton = $(`<button type="button" class="button">+</button>`).on('click', addClassSelector);
+    form.append(autocomplete).append(addAutoButton);
+    formView.append(form);
+    
+
+    return formView;
+}
+
+function addClassSelector() {
+    console.log(counter);
+    let target = $(`#div-${counter}`);
+    let inputDiv = $(`<div id='div-${counter}' class='is-flex-direction-row control field'></div>`);
+    let autocomplete = $(`<input class="input" type="text" id="className" placeholder="Enter Class">`).css('width', '500px');
+    let addAutoButton = $(`<button type="button" class="button">+</button>`).on('click', addClassSelector);
+    inputDiv.append(autocomplete).append(addAutoButton);
+    console.log($(`#div-${counter}`)[0]);
+    target.after(inputDiv);
+    counter++;
 }
 
 function renderClassView(c) {
     let $classView = $('<div class="class_box" id="class-display"></div>');
-    let $className = $(`<h1 class="has-text-centered">${c.name}</h1><hr>`);
+    let $className = $(`<h5 class="has-text-centered title is-1">${c.name}</h5>`);
     $classView.append($className);
+    //let buttons
     if (c.exam_dates.length > 0) {
-        $className.append('<h2>Exam Dates</h2>');
+        $className.append('<hr><h1 class="subtitle is-2">Exam Dates</h1>');
         c.exam_dates.forEach((date) => {
-            let $date = $(`<p>${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}</p>`);
+            //let $dateDiv = $('<div class="is-flex-direction-column is-align-content-space-between"></div>')
+            let $date = $(`<p class="subtitle is-5">${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}</p>`);
             $className.append($date);
         });
     } else {
         $className.append("<h3>There are no exam dates set up for this class.</h3>")
     }
     $root.append($classView);
-
 }
 
-function renderAddClassForm(event) {
-    $("#new-class-button").hide();
-    let $classesView = $('<div class="class_box" id="class_display"></div>');
-    let $classInfoForm = $("<form id='create-class-form'></form>");
-    $classesView.append($classInfoForm);
-    let $classNameDropdown = $(`<input type="text" id="className" placeholder="Enter Class"><hr>`);
-    $classInfoForm.append($classNameDropdown);
+let $classNameDropdown = $(`<input type="text" id="className" placeholder="Enter Class"><hr>`);
 
-    $root.append($classesView)
-    $("#new-class-button").on('click', autocomplete(document.getElementById("className"), classes.map((c) => c.name)))
-}
+// function renderAddClassForm(event) {
+//     $("#new-class-button").remove();
+//     let $classesView = $('<div class="class_box" id="class_display"></div>');
+//     let $classInfoForm = $("<form id='create-class-form'></form>");
+//     $classesView.append($classInfoForm);
 
+//     $classNameDropdown.append($("<button class='button ' type='button'>+</button"));
 
-// // ===============================AUTOCOMPLETE==================================
-function autocomplete(myInput, classArray) {
-    myInput.addEventListener("input", function (e) {
-        minimizeOptions();
-        var val = this.value;
-        if (!val) {
-            return;
-        }
+//     $classInfoForm.append($classNameDropdown);
 
-        var a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        this.parentNode.appendChild(a);
-
-        for (var i = 0; i < classArray.length; i++) {
-            if (classArray[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                var b = document.createElement("DIV");
-                b.innerHTML = "<strong>" + classArray[i].substr(0, val.length) + "</strong>";
-                b.innerHTML += classArray[i].substr(val.length);
-                b.innerHTML += "<input type='hidden' value='" + classArray[i] + "'>";
-                b.addEventListener("click", function (e) {
-                    myInput.value = this.getElementsByTagName("input")[0].value;
-                    minimizeOptions();
-                });
-                a.appendChild(b);
-            }
-        }
-    });
+//     $("#add-class-view").append($classesView)
 
 
 
-    function minimizeOptions(item) {
-        var classList = document.getElementsByClassName("autocomplete-items");
-        for (var i = 0; i < classList.length; i++) {
-            if (item != classList[i] && item != myInput) {
-                classList[i].parentNode.removeChild(classList[i]);
-            }
-        }
-    }
+// }
 
-}
 
-function debounce(func, wait, immediate) {
-    var timeout;
-    return function () {
-        var context = this, args = arguments;
-        var later = function () {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-};
-
-var incorporateDebounce = debounce(function () {
-    let classList = [];
-    classes.forEach((c) => classList.push(c.name));
-    autocomplete(document.getElementById("className"), classList)
-}, 250);
 
 
 
@@ -186,10 +159,10 @@ var incorporateDebounce = debounce(function () {
 // }
 
 
-/**  let prof = axios request to get prof object 
+/**  let prof = axios request to get prof object
  *  prof.classes.forEach((c) => {
  *      let numberOfOtherExamsStudentHave - c.forEach((s) => {
- *             
+ *
  *  })
  * })
  * */
--- professorView.js
+++ professorView.js
@@ -1,61 +1,19 @@
 
-let initializeCalendar =  (date, total)=> {
-  //var finalDate = date;
-  var finalTotal = total;
-    // Initialize random data for the demo
-    var now = new Date (2020,10,25);
-      var time_ago = new Date (2020, 7, 7)
-      var example_data = d3.timeDays(time_ago, now).map(function (finalDate, index) {
-    var finalDate = date;
-        return {
-          date: finalDate,
-          details: Array.apply(null, new Array(Math.floor(Math.random() * 15))).map(function(e, i, arr) {
-            return {
-              'name': '',
-              'date': finalDate,
-              'value': 3600 * ((arr.length - i) / 5) + Math.floor(Math.random() * 3600) * Math.round(Math.random() * (index / 365))
-            }
-          }),
-          init: function () {
-            this.total = finalTotal;
-            return this;
-        }
-      }.init();
-    });
-    
 
-    // Set the div target id
-    var div_id = 'calendar';
 
-    // Set custom color for the calendar heatmap
-    var color = '#cd2327';
-
-    // Set overview type (choices are year, month and day)
-    var overview = 'year';
-
-    // Handler function
-    var print = function (val) {
-      console.log(val);
-    };
-
-    // Initialize calendar heatmap
-    calendarHeatmap.init(example_data, div_id, color, overview, print);
-  };
-
-  export const renderDropDownClasses = function(Professor){
-    for(let i =0; i < Professor[0].classes.length; i++){
-      let classDrop = Professor[0].classes[i].name;
-      $(".options").append(`<a href="">${classDrop}</a>`);
-    }
-    
-
-    
+export const renderDropDownClasses = function (Professor) {
+  for (let i = 0; i < Professor[0].classes.length; i++) {
+    let classDrop = Professor[0].classes[i].name;
+    $(".options").append(`<a href="">${classDrop}</a>`);
   }
+  $(".options").append(`<a href="./profDash.html">+ Add Class</a>`);
 
-  export const loadProfessorView = function(Professor) {    
-    const $root = $('#root');    
-    $("#root").on('mouseover','.dropdown', renderDropDownClasses(Professor));   
- }
+
+}
+
+export const loadProfessorView = function (Professor) {
+  $("#root").on('mouseover', '.dropdown', renderDropDownClasses(Professor));
+}
 
 
  async function getProfessor() {
@@ -90,6 +48,7 @@ function getPassedIn(Professor) {
 
 
 $(function () {
+  $("#signout-button").on('click', signout);
   // let findLoggedInProf = async function(id){
   //   let req = await axios({
   //     method: "get",
@@ -97,91 +56,90 @@ $(function () {
   //   })
   //   console.log(req); 
   // }
-  let Professor = 
-{
-  "0": {
-        "id": 0,
-        "firstName": "Kaitlyn",
-        "lastName": "Mirabella",
-        "email": "test@unc.edu",
-        "user": "kaitlyn",
-        "classes": [
+  let Professor =
+  {
+    "0": {
+      "id": 0,
+      "firstName": "Kaitlyn",
+      "lastName": "Mirabella",
+      "email": "test@unc.edu",
+      "user": "kaitlyn",
+      "classes": [
+        {
+          "id": 0,
+          "name": "comp110_001",
+          "exam_dates": [
             {
-                "id": 0,
-                "name": "comp110_001",
-                "exam_dates": [
-                    {
-                        "day": "9",
-                        "month": "2",
-                        "year": "2020",
-                        "total": "1"
-                    },
-                    {
-                        "day": "10",
-                        "month": "11",
-                        "year": "2020",
-                        "total": "3"
-                    },
-                    {
-                        "day": "11",
-                        "month": "3",
-                        "year": "2020",
-                        "total": "1"
-                    }
-                ], 
-                "students": [
-                    {
-                        "password": "comp123",
-                        "name": "Kaitlyn",
-                        "type": "Student"
-                    },
-                    {
-                        "password": "comp123",
-                        "name": "Kush",
-                        "type": "student"
-                    }
-                ]
+              "day": "9",
+              "month": "24",
+              "year": "2020"
             },
             {
-                "id": 1,
-                "name": "comp426_001",
-                "exam_dates": [
-                    {
-                        "day": "19",
-                        "month": "4",
-                        "year": "2020",
-                        "total": "150"
-                    },
-                    {
-                        "day": "30",
-                        "month": "8",
-                        "year": "2020",
-                        "total": "15"
-                    },
-                    {
-                        "day": "2",
-                        "month": "1",
-                        "year": "2020",
-                        "total": "59"
-                    }
-                ], 
-                "students": [
-                    {
-                        "password": "comp123",
-                        "name": "Kaitlyn",
-                        "type": "Student"
-                    },
-                    {
-                        "password": "comp123",
-                        "name": "Kush",
-                        "type": "student"
-                    }
-                ]
+              "day": "10",
+              "month": "11",
+              "year": "2020"
+            },
+            {
+              "day": "11",
+              "month": "26",
+              "year": "2020"
             }
-        ]
+          ],
+          "students": [
+            {
+              "password": "comp123",
+              "name": "Kaitlyn",
+              "type": "Student"
+            },
+            {
+              "password": "comp123",
+              "name": "Kush",
+              "type": "student"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "comp426_001",
+          "exam_dates": [
+            {
+              "day": "9",
+              "month": "24",
+              "year": "2020"
+            },
+            {
+              "day": "10",
+              "month": "11",
+              "year": "2020"
+            },
+            {
+              "day": "11",
+              "month": "26",
+              "year": "2020"
+            }
+          ],
+          "students": [
+            {
+              "password": "comp123",
+              "name": "Kaitlyn",
+              "type": "Student"
+            },
+            {
+              "password": "comp123",
+              "name": "Kush",
+              "type": "student"
+            }
+          ]
+        }
+      ]
     }
-};
-initializeCalendar(new Date(2020,4,5),5);
-getPassedIn(Professor);
-loadProfessorView(Professor);    
+  };
+  loadProfessorView(Professor);
 });
+
+async function signout() {
+  await axios({
+    method: "GET",
+    url: "https://comp426backend.herokuapp.com/logout"
+  })
+}
